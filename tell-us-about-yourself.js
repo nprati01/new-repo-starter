056e6bf1080f6e7e3f5b911e3e650c4f5820bad8
Joshua W Smith <ws.joshua@gmail.com>
2023-02-13 14:32:09 -0700

    Added ice breaker questions

---
+++ tell-us-about-yourself.js
@@ -0,0 +1,11 @@
+// Tell us about yourself 
+
+// 1. Where do you call home? 
+
+// 2. Have you lived anywhere else? If so where? 
+
+// 3. What was your most recent job? What did you enjoy about that role?
+
+// 4. What is your "comfort" food? 
+
+// 5. What is one thing you find interesting about yourself ( that others might not notice at first) ? 
