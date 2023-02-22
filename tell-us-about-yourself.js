9de71cd0cdeb22a996f679bcb4a4e5cdcbd46c45
nprati01 <nicole.e.prati@gmail.com>
2023-02-22 09:36:18 -0500

    I added my answers

--- tell-us-about-yourself.js
+++ tell-us-about-yourself.js
@@ -1,11 +1,16 @@
-// Tell us about yourself 
+// Tell us about yourself
 
-// 1. Where do you call home? 
+// 1. Where do you call home?
+Houston Texas will always be my home but I live in Florida
 
-// 2. Have you lived anywhere else? If so where? 
+// 2. Have you lived anywhere else? If so where?
+San Marcos Tx and Virginia
 
 // 3. What was your most recent job? What did you enjoy about that role?
+After loosing my career job during the pandeming; I began  working as a bartender- I loved it. I got to meet a lot of really cool people and learn alot from others
 
-// 4. What is your "comfort" food? 
+// 4. What is your "comfort" food?
+Pho
 
-// 5. What is one thing you find interesting about yourself ( that others might not notice at first) ? 
+// 5. What is one thing you find interesting about yourself ( that others might not notice at first) ?
+im boring 
